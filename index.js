5d00104b96668e0e310eebdff2b77f53c634ab2f
shinji5761 <shinji.fsi.5761@gmail.com>
2018-03-21 15:23:28 +0900

    CrossOriginの許可

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ var app = express();
 var PORT = process.env.PORT || 5000;
 var Main = (function () {
     function Main() {
+        this.middleware();
         this.routing();
         this.listen();
     }
     Main.prototype.middleware = function () {
+        // Cross Originの許可
+        app.use(function (req, res, next) {
+            res.header('Access-Control-Allow-origin', '*');
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
     };
     Main.prototype.routing = function () {
         app.get('/', function (request, response) { response.send('HelloWorld'); });
